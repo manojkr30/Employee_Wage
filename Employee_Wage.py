1f5565f7f90cd52e9181db77cb66ae5c46ea01e1
manojkr30 <manojramesh3001@gmail.com>
2024-08-01 16:43:17 +0530

    [Manoj KR]  Add:UC-4

--- Employee_Wage.py
+++ Employee_Wage.py
@@ -7,7 +7,7 @@
 
 @Last Modified time: 2024-08-01 13:38:11
 
-@Title : Calculate Daily Employee Wage .
+@Title : Calculate Daily Employee Wage  .
 """  
 import random
 def display_welcome():
@@ -59,23 +59,34 @@ def calculate_wage_of_employee():
     parameters:
                None.
     return:
+            By calling switch_statment by sending attendence and fulltime or partime by o and 1. 
            160 -> if employee Present and Full Time.
            80 -> if employe Present and Part Time.
            0 -> if employee Absent.
     """
-    
-    if check_attendance() == 1 :
-        print("Employee Present Today")
-        if check_full_or_part_time() == 1 :
-            print("for Full Time")
+    return swith_statment([check_attendance(),check_full_or_part_time()])
+def swith_statment(match_values):
+    """
+     description:
+        This function is used to give Calculate Daily Employee Wage for both Full Time and Part time Employee by using Switch Statment.
+    parameters:
+            list : having attendance and parttime or fulltime.
+    return: 
+           160 -> if employee Present and Full Time.
+           80 -> if employe Present and Part Time.
+           0 -> if employee Absent.
+    """
+    match match_values:
+        case [1,1] : 
+            print("Employee Present Today")
             return 8*20
-        else :
+        case [1,0] :
             print("for Part time")
             return 4*20
-    else:
-        print("Employee Absent Today")
-        return 0
-    
+        case _:
+            print("Employee Absent Today")
+            return 0
+            
 if __name__ =="__main__":
     display_welcome()
     empoyee_wage=0
